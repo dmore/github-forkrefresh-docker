7e381f74ab1ccbddd776e4f35d296df17f8b8103
Dario More Escamez <dmore@users.noreply.github.com>
2023-09-26 11:39:07 +0200

    go fmt
    standardised go formatting. builds ok locally, so pushing.

--- app/main.go
+++ app/main.go
@@ -1,80 +1,80 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"github.com/Jeffail/gabs"
+	"github.com/zalando/go-keyring"
+	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
-	"time"
-	"io/ioutil"
-	"io"
-	"log"
 	"path/filepath"
-	"encoding/json"
 	"strings"
-	"github.com/zalando/go-keyring"
-	"github.com/Jeffail/gabs"
-	"bytes"
+	"time"
 )
 
 var token_variable = ""
 var logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
-//var logfile
+
+// var logfile
 var keychain_app_service = "github-forkrefresh1"
 var keychain_username = "dmore1"
 var app_log_file = "app.log"
 
-//log to std out
-func setup_logging(){
+// log to std out
+func setup_logging() {
 
 	//logger := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
-	var logfile, err  = os.Create(app_log_file)
+	var logfile, err = os.Create(app_log_file)
 
-    if err != nil {
-     log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    defer logfile.Close()
+	defer logfile.Close()
 
-    log.SetOutput(logfile)
-    //aiming to also log to console
-    log.SetOutput(os.Stdout)
-    log.SetOutput(os.Stderr)
+	log.SetOutput(logfile)
+	//aiming to also log to console
+	log.SetOutput(os.Stdout)
+	log.SetOutput(os.Stderr)
 
 }
 
-//this method stores your secret on the OS keychain. 
-func store_secret_on_keychain(token string ){
+// this method stores your secret on the OS keychain.
+func store_secret_on_keychain(token string) {
 
 	service := keychain_app_service
-    user := keychain_username
-    password := token
-    //if you want to inject it onto or from an env var...
-    //password := token
-    //os.Setenv("GITHUB_TOKEN",password)
-    //password = os.Getenv("GITHUB_TOKEN")
-  
-    // set password
-    err := keyring.Set(service, user, password)
-    if err != nil {
-        log.Fatal(err)
-    }
+	user := keychain_username
+	password := token
+	//if you want to inject it onto or from an env var...
+	//password := token
+	//os.Setenv("GITHUB_TOKEN",password)
+	//password = os.Getenv("GITHUB_TOKEN")
+
+	// set password
+	err := keyring.Set(service, user, password)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-//this method retrieves secret from the keychain. 
-func retrieve_secret_from_keychain() (string){
+// this method retrieves secret from the keychain.
+func retrieve_secret_from_keychain() string {
 
 	service := keychain_app_service
-    user := keychain_username
+	user := keychain_username
 
 	// get password
-    secret, err := keyring.Get(service, user)
-    if err != nil {
-        log.Fatal(err)
-    }
+	secret, err := keyring.Get(service, user)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    return secret
+	return secret
 }
-
 
 func main() {
 
@@ -83,15 +83,15 @@ func main() {
 
 	//keychain app service from env var.
 	keychain_app_service = os.Getenv("KEYCHAIN_APP_SERVICE")
-	if (len(keychain_app_service) == 0 ) {
-        //log.Fatal(err)
-        os.Exit(1)
-    }
+	if len(keychain_app_service) == 0 {
+		//log.Fatal(err)
+		os.Exit(1)
+	}
 	log.Println("keychain_app_service:" + keychain_app_service)
 
 	//keychain username from env var
 	keychain_username = os.Getenv("KEYCHAIN_USERNAME")
-	if (len(keychain_username) == 0){
+	if len(keychain_username) == 0 {
 		//log.Fatal(err)
 		os.Exit(1)
 	}
@@ -101,10 +101,10 @@ func main() {
 	//store_secret_on_keychain("GITHUB_TOKEN")
 
 	var github_token_env_var = os.Getenv("GITHUB_TOKEN")
-	if (len(github_token_env_var) == 0){
+	if len(github_token_env_var) == 0 {
 		//pull it from the keychain instead.
 		token_variable = retrieve_secret_from_keychain()
-	}else{
+	} else {
 		//use passed in token if non-empty string
 		token_variable = github_token_env_var
 	}
@@ -113,15 +113,15 @@ func main() {
 
 	var arr []string
 	var err error
-	//var getErr = nil 
-	//var readErr = nil 
-	
-	if (len(github_gist) > 0){
+	//var getErr = nil
+	//var readErr = nil
+
+	if len(github_gist) > 0 {
 
 		log.Println("downloading repos list from gist")
 		log.Println("" + github_gist)
 		var url = github_gist
-		
+
 		var gistClient = http.Client{
 			Timeout: time.Second * 2, // Timeout after 2 seconds
 		}
@@ -152,63 +152,61 @@ func main() {
 			log.Fatal(readErr)
 			os.Exit(1)
 		}
-		
+
 		var err3 = json.Unmarshal([]byte(body), &arr)
 		if err3 != nil {
-	      fmt.Println("error3:", err3)
-	      os.Exit(1)
-	    }
-	}else{
+			fmt.Println("error3:", err3)
+			os.Exit(1)
+		}
+	} else {
 		/** using repos_repo.json file **/
 		//refactor to pull dynamically.
 		//file must be json array not json
 		absPath, _ := filepath.Abs("repos_repo.json")
 		f, err := os.Open(absPath)
 		if err != nil {
-		    log.Fatal(err)
+			log.Fatal(err)
 		}
 		log.Println("Successfully Opened repos_repo.json")
 		defer f.Close()
 
 		//unmarshall
-		byteValue, _ := ioutil.ReadAll(f)   
+		byteValue, _ := ioutil.ReadAll(f)
 		err3 := json.Unmarshal([]byte(byteValue), &arr)
 		if err3 != nil {
-	      fmt.Println("error3:", err3)
-	      os.Exit(1)
-	    }
+			fmt.Println("error3:", err3)
+			os.Exit(1)
+		}
 	}
 
-	
-	//var result map[string]interface{} not working. 
+	//var result map[string]interface{} not working.
 	//in memory test works fine also
 	/**
-	var dataJson = `[
-    	"dmore/aws-vault-local-os-keychain-mfa",
-    	"dmore/aws-multi-region-cicd-with-terraform"
-	]`
-	err2 := json.Unmarshal([]byte(dataJson), &arr)
-	if err2 != nil {
-      fmt.Println("error2:", err2)
-      os.Exit(1)
-    }
-    **/
-	
-	
-    log.Printf("Unmarshaled: %s", arr)
-    //loop through
-    for i := 0; i < len(arr); i++ {
-		
+		var dataJson = `[
+	    	"dmore/aws-vault-local-os-keychain-mfa",
+	    	"dmore/aws-multi-region-cicd-with-terraform"
+		]`
+		err2 := json.Unmarshal([]byte(dataJson), &arr)
+		if err2 != nil {
+	      fmt.Println("error2:", err2)
+	      os.Exit(1)
+	    }
+	    **/
+
+	log.Printf("Unmarshaled: %s", arr)
+	//loop through
+	for i := 0; i < len(arr); i++ {
+
 		var reponame = string(arr[i])
 		reponame = strings.TrimSuffix(reponame, "/")
 		reponame = strings.TrimPrefix(reponame, "/")
 		fmt.Println(reponame)
-		
+
 		//LOOP HERE EACH ELEMENT
 
-	    var ret = ""
-	    var branch = ""
-	    //grab branches first so we know what branch name we need in advance...
+		var ret = ""
+		var branch = ""
+		//grab branches first so we know what branch name we need in advance...
 		branch, err = fork_get_query_branch(reponame)
 		if err != nil {
 			log.Fatalln(err)
@@ -229,11 +227,11 @@ func main() {
 			log.Println("Branch Not Found so we will skip this one ")
 			log.Println("branch not there....repo does not exist...")
 			continue
-		}else{
+		} else {
 			log.Println("branch found ok")
 		}
-	    //relevant branch call
-		ret,err = fork_refresh_call(branch, reponame, "POST")
+		//relevant branch call
+		ret, err = fork_refresh_call(branch, reponame, "POST")
 		if err != nil {
 			log.Fatalln(err)
 			//ret2,err2 := call("", "main", "", "POST")
@@ -249,14 +247,14 @@ func main() {
 		log.Println("- - - - - - -- - - - - -- - -- - -")
 		log.Println(string(ret))
 		log.Println("- - - - - - -- - - - - -- - -- - -")
-		
+
 		if strings.Contains(string(ret), "Not Found") {
 			log.Println("checking...")
 			log.Println(string(ret))
 
 			log.Println("Not Found found on stringifed response => [main]")
 			//main call
-			ret,err = fork_refresh_call("main", reponame, "POST")
+			ret, err = fork_refresh_call("main", reponame, "POST")
 			if err != nil {
 				log.Fatalln(err)
 				//ret2,err2 := call("", "main", "", "POST")
@@ -267,11 +265,11 @@ func main() {
 			}
 			log.Println("notok3")
 			continue
-		}else{
+		} else {
 			log.Println("ok")
 			continue
 		}
-		
+
 	}
 	//exit after looping repo names
 	log.Println("Done")
@@ -280,7 +278,6 @@ func main() {
 	os.Exit(0)
 
 }
-
 
 func fork_get_query_branch(reponame string) (string, error) {
 
@@ -291,16 +288,16 @@ func fork_get_query_branch(reponame string) (string, error) {
 	fmt.Println("url: " + string(httpposturl))
 	request, err := http.NewRequest("GET", httpposturl, nil)
 	if err != nil {
-	    log.Fatal(err)
+		log.Fatal(err)
 	}
 	//request.Header.Set("Content-Type", "application/json; charset=UTF-8")
 	request.Header.Set("Accept", "Accept: application/vnd.github+json")
-	request.Header.Set("Authorization", "token " + token_variable)
+	request.Header.Set("Authorization", "token "+token_variable)
 	request.Header.Set("X-GitHub-Api-Version", "2022-11-28")
 
 	response, err := http.DefaultClient.Do(request)
 	if err != nil {
-	    log.Fatal(err)
+		log.Fatal(err)
 	}
 	defer response.Body.Close()
 
@@ -321,11 +318,11 @@ func fork_get_query_branch(reponame string) (string, error) {
 
 	var objmap []map[string]interface{}
 	if err := json.Unmarshal(b, &objmap); err != nil {
-    	log.Fatal(err)
-    	//branch is not there, so blows up
-    	return "unable_to_unmarshall_branch_response", err
+		log.Fatal(err)
+		//branch is not there, so blows up
+		return "unable_to_unmarshall_branch_response", err
 	}
-	if (len(objmap) == 0){
+	if len(objmap) == 0 {
 		return "failed to log", err
 	}
 	log.Println("Unmarshalled")
@@ -333,20 +330,20 @@ func fork_get_query_branch(reponame string) (string, error) {
 
 	var return_branch = ""
 	for k, v := range objmap[0] {
-	    switch c := v.(type) {
-	    case string:
-	    	if k == "name" {
-	    		return_branch = string(c)
-	    		fmt.Printf("Item %q is a string, containing %q\n", k, c)
-	    	}
-	        
-	    case float64:
-	        //fmt.Printf("Looks like item %q is a number, specifically %f\n", k, c)
-	        continue
-	    default:
-	        //fmt.Printf("Not sure what type item %q is, but I think it might be %T\n", k, c)
-	        continue
-	    }
+		switch c := v.(type) {
+		case string:
+			if k == "name" {
+				return_branch = string(c)
+				fmt.Printf("Item %q is a string, containing %q\n", k, c)
+			}
+
+		case float64:
+			//fmt.Printf("Looks like item %q is a number, specifically %f\n", k, c)
+			continue
+		default:
+			//fmt.Printf("Not sure what type item %q is, but I think it might be %T\n", k, c)
+			continue
+		}
 	}
 	fmt.Println("return_branch is " + return_branch)
 	return string(return_branch), nil
@@ -354,11 +351,11 @@ func fork_get_query_branch(reponame string) (string, error) {
 
 func fork_refresh_call(branch string, reponame string, method string) (string, error) {
 	//now that we know the branch name in advance we can use that instead of this.
-	
+
 	jsonObj := gabs.New()
 	// or gabs.Wrap(jsonObject) to work on an existing map[string]interface{}
 
-	jsonObj.Set("" + branch, "branch")
+	jsonObj.Set(""+branch, "branch")
 
 	jsonOutput := jsonObj.String()
 
@@ -366,7 +363,7 @@ func fork_refresh_call(branch string, reponame string, method string) (string, e
 	fmt.Println(jsonObj.StringIndent("", "  "))
 
 	var jsonStr = []byte(jsonOutput)
-   
+
 	reponame = strings.TrimSuffix(reponame, "/")
 	reponame = strings.TrimPrefix(reponame, "/")
 
@@ -374,15 +371,15 @@ func fork_refresh_call(branch string, reponame string, method string) (string, e
 	//fmt.Println("url: %s", (string)httpposturl)
 	request, err := http.NewRequest("POST", httpposturl, bytes.NewBuffer(jsonStr))
 	if err != nil {
-	    log.Fatal(err)
+		log.Fatal(err)
 	}
 	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
 	request.Header.Set("Accept", "application/vnd.github.v3+json")
-	request.Header.Set("Authorization", "token " + token_variable)
+	request.Header.Set("Authorization", "token "+token_variable)
 
 	response, err := http.DefaultClient.Do(request)
 	if err != nil {
-	    log.Fatal(err)
+		log.Fatal(err)
 	}
 	defer response.Body.Close()
 
--- app/main_test.go
+++ app/main_test.go
@@ -5,41 +5,42 @@ import (
 	//"os",
 )
 
-import ("os")
+import (
+	"os"
+)
 
 func Test1(t *testing.T) {
 }
 
+func Test_KEYCHAIN_APP_SERVICE(t *testing.T) {
 
-func Test_KEYCHAIN_APP_SERVICE(t *testing.T){
+	got := os.Getenv("KEYCHAIN_APP_SERVICE")
+	want := "github-forkrefresh"
 
-    got := os.Getenv("KEYCHAIN_APP_SERVICE")
-    want := "github-forkrefresh"
-
-    if got != want {
-        t.Errorf("got %q, wanted %q", got, want)
-    }
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
 }
 
-func Test_KEYCHAIN_USERNAME(t *testing.T){
+func Test_KEYCHAIN_USERNAME(t *testing.T) {
 
-    got := os.Getenv("KEYCHAIN_USERNAME")
-    want := "dmore"
+	got := os.Getenv("KEYCHAIN_USERNAME")
+	want := "dmore"
 
-    if got != want {
-        t.Errorf("got %q, wanted %q", got, want)
-    }
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
 }
 
-func TestGITHUB_TOKEN(t *testing.T){
+func TestGITHUB_TOKEN(t *testing.T) {
 
-    //got := os.Getenv("GITHUB_TOKEN")
+	//got := os.Getenv("GITHUB_TOKEN")
 
-    //if (len(got) == 0){
-    //	result = main.retrieve_secret_from_keychain("test", "test")
-    //}
-   
-    //if (len(got) == 0) {
-    //    t.Errorf("token not passed as env var")
-   // }
+	//if (len(got) == 0){
+	//	result = main.retrieve_secret_from_keychain("test", "test")
+	//}
+
+	//if (len(got) == 0) {
+	//    t.Errorf("token not passed as env var")
+	// }
 }
